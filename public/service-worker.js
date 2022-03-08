cc8641839a0b1ff804669cf9d287fb3179e982ad
Massimo Moffa <massimomoffa02@gmail.com>
2022-03-08 16:00:40 +0100

    nodenod

--- public/service-worker.js
+++ public/service-worker.js
@@ -50,33 +50,15 @@ self.addEventListener("fetch", (evt) => {
 // Register event listener for the 'push' event.
 self.addEventListener('push', function(event) {
     console.log("NOTIFICATION!");
-    console.log(event);
+    let data = event.data.json();
     event.waitUntil(
       // Retrieve a list of the clients of this service worker.
       self.clients.matchAll().then(function(clientList) {
-        // Check if there's at least one focused client.
-        var focused = clientList.some(function(client) {
-          return client.focused;
+        
+        return self.registration.showNotification(data.title, {
+          body: data.body,
         });
-  
-        var notificationMessage;
-        if (focused) {
-          notificationMessage = 'You\'re still here, thanks!';
-        } else if (clientList.length > 0) {
-          notificationMessage = 'You haven\'t closed the page, ' +
-                                'click here to focus it!';
-        } else {
-          notificationMessage = 'You have closed the page, ' +
-                                'click here to re-open it!';
-        }
-  
-        // Show a notification with title 'ServiceWorker Cookbook' and body depending
-        // on the state of the clients of the service worker (three different bodies:
-        // 1, the page is focused; 2, the page is still open but unfocused; 3, the page
-        // is closed).
-        return self.registration.showNotification('ServiceWorker Cookbook', {
-          body: notificationMessage,
-        });
+
       })
     );
   });
