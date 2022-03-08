53b41a52d20ca53ee76365b6a8de7987d9eedbba
Massimo Moffa <massimomoffa02@gmail.com>
2022-03-08 15:39:45 +0100

    first commit

---
+++ public/service-worker.js
@@ -0,0 +1,82 @@
+"use strict";
+// Cache Name
+const CACHE_NAME = "static-cache-v1";
+// Cache Files
+const FILES_TO_CACHE = ["/offline.html"];
+// install
+self.addEventListener("install", (evt) => {
+  console.log("[ServiceWorker] Install");
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("[ServiceWorker] Pre-caching offline page");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+// Active PWA Cache and clear out anything older
+self.addEventListener("activate", (evt) => {
+  console.log("[ServiceWorker] Activate");
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+// listen for fetch events in page navigation and return anything that has been cached
+self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
+  // when not a navigation event return
+  if (evt.request.mode !== "navigate") {
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(() => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match("offline.html");
+      });
+    })
+  );
+});
+
+// Register event listener for the 'push' event.
+self.addEventListener('push', function(event) {
+    console.log("NOTIFICATION!");
+    console.log(event);
+    event.waitUntil(
+      // Retrieve a list of the clients of this service worker.
+      self.clients.matchAll().then(function(clientList) {
+        // Check if there's at least one focused client.
+        var focused = clientList.some(function(client) {
+          return client.focused;
+        });
+  
+        var notificationMessage;
+        if (focused) {
+          notificationMessage = 'You\'re still here, thanks!';
+        } else if (clientList.length > 0) {
+          notificationMessage = 'You haven\'t closed the page, ' +
+                                'click here to focus it!';
+        } else {
+          notificationMessage = 'You have closed the page, ' +
+                                'click here to re-open it!';
+        }
+  
+        // Show a notification with title 'ServiceWorker Cookbook' and body depending
+        // on the state of the clients of the service worker (three different bodies:
+        // 1, the page is focused; 2, the page is still open but unfocused; 3, the page
+        // is closed).
+        return self.registration.showNotification('ServiceWorker Cookbook', {
+          body: notificationMessage,
+        });
+      })
+    );
+  });
---
+++ src/App.svelte
@@ -0,0 +1,124 @@
+<script>
+	import { onMount } from "svelte";
+
+	let swreg = null;
+	let permission = false;
+
+	onMount(() => {
+
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.register('/service-worker.js').then(function(reg) {
+
+				if(reg.installing) {
+					console.log('Service worker installing');
+				} else if(reg.waiting) {
+					console.log('Service worker installed');
+				} else if(reg.active) {
+					console.log('Service worker active');
+				}
+
+			}).catch(function(error) {
+				console.log('Registration failed with ' + error);
+			});
+		}
+
+		navigator.serviceWorker.ready.then(serviceWorkerRegistration => {
+			swreg = serviceWorkerRegistration;
+			console.log(swreg);
+		});
+
+	});
+
+	function askNotification() {
+		if (!("Notification" in window)) {
+			alert("This browser does not support desktop notification");
+		}
+		Notification.requestPermission().then(function (_permission) {
+			permission = _permission === "granted";
+			if(permission){
+
+				var options = {
+					userVisibleOnly: true,
+					applicationServerKey: urlBase64ToUint8Array("BFaNs8n5JYn2ozrR2z1-FfuNXSvCBQxyUPvR1Z1uNQS0c8ZLS2qJhDAH_U9IiFkECR5WOGBP4aw9scgHZazDvNg")
+				};
+				swreg.pushManager.subscribe(options).then(
+				function(pushSubscription) {
+					console.log(pushSubscription);
+				}, function(error) {
+					console.log(error);
+				}
+				);
+
+			}
+		});
+	}
+
+	function urlBase64ToUint8Array(base64String) {
+		var padding = '='.repeat((4 - base64String.length % 4) % 4);
+		var base64 = (base64String + padding)
+			.replace(/\-/g, '+')
+			.replace(/_/g, '/');
+
+		var rawData = window.atob(base64);
+		var outputArray = new Uint8Array(rawData.length);
+
+		for (var i = 0; i < rawData.length; ++i) {
+			outputArray[i] = rawData.charCodeAt(i);
+		}
+		return outputArray;
+	}
+
+</script>
+
+<main>
+	
+	<div class="btn-notification" on:click={askNotification}>
+
+		<span>
+
+			Richiedi notifiche
+
+		</span>
+
+	</div>
+
+	<div class="status-permission" {permission}>
+		{permission ? "Notifiche abilitate!" : "Notifiche disabilitate!"}
+	</div>
+	
+</main>
+
+<style>
+
+	main {
+		width: 100%;
+		height: 100%;
+		display: grid;
+		place-items: center;
+	}
+
+	main > .btn-notification {
+		width: auto;
+		height: auto;
+		padding: 6px;
+		border-radius: 3px;
+		background-color: burlywood;
+		cursor: pointer;
+		user-select: none;
+		transition: 0.3s ease all;
+	}
+
+	main > .btn-notification:hover {
+		box-shadow: 0 2px 6px grey;
+		opacity: 0.6;
+	}
+
+	main > .status-permission {
+		color: red
+	}
+
+	main > .status-permission[permission=true] {
+		color: green
+	}
+
+</style>
